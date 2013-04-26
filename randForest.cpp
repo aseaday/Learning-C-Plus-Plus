bd44df7650353a7b4a235d2f14c4f0875edfc912
Abraham Xiao <q289196573@gmail.com>
2013-04-26 12:40:50 +0800

    Source Code Library Updat
    Updated at 12:40.
    Fuck it......
    Have to eat something other than the "Beef Gan".
    
    Well, some errors hapened when I opened, and operated on those files.
    
    The former two, namely the randFeature.h & randFeature.cpp, I did ADD
    some comments into them......But there was a backup. And I made a big
    mistake when I copy those files.
    
    The comments were flushed......What the FUCK.
    Form a better habit of backup,synchronizing habit.
    And!!! For codes, save & edit them in the Git folder.
    
    Commit then more frequently, thus you are much at ease to figure out...
    the Changes!!!!!
    
    By Abraham Xiao @ InfoSec Lab, School of Automation.
    Northwestern Polytechnic University.

--- randForest.cpp
+++ randForest.cpp
@@ -1,14 +1,28 @@
+/*
+  This is a big boy~ Aha do not worry, if you would
+  like to be a programmer (software engineer), tis just 
+  the beginning.
+ */
+
 #include "randForest.h"
 
 
 
+
 //..
-pslNodeList*     nodeList    = 0;       // the node list of the tree
+pslNodeList* nodeList = 0;       // the node list of the tree
 //..
 pslNode *gbTree = 0;
 
 //extern unsigned char* feaMat;
 
+
+/*
+  You should visit sites like http://github.com more 
+  often to figure out how those great hackers write codes.
+  When dealing with pointers, the if (!XXX) is never a good 
+  practice. Some bugs may occur once your program goes bigger.
+ */
 pslNode* allocateNode(){
 
 	if(!nodeList)
@@ -18,13 +32,17 @@ pslNode* allocateNode(){
 	node->nodeID = (int)nodeList->size();
 	nodeList->push_back(node);
 	return (*nodeList)[nodeList->size()-1];
+	/*
+	  To better understand those codes, read some sample
+	  codes on LinkList & Tree. (in Data Structure books)
+	 */
 };
 float getEntropyLUT(float* LUT, int len){
 	float entropy = 0.f;
 
 	for(int i = 0; i < len; i++){
 		entropy += (float) (LUT[i]*log(LUT[i]+EPS)/log(2.0));
-	}
+	}  /* Just the implementation of pre-defined get-entropy algo. */
 
 	return -entropy;
 };
@@ -47,11 +65,17 @@ int getNodeEntropy(pslNode* node){
 	node->pointNum = 0;
 	memset(node->LUT, 0, LUT_BIN_NUM*sizeof(float));
 	int pointIdx = node->pointLink;
-	while(pointIdx >= 0){
+	while (pointIdx >= 0){  
 		int label = (*keypointList)[pointIdx]->label;
 		node->LUT[label]++;
 		node->pointNum++;
-		pointIdx = (*keypointList)[pointIdx]->link;
+		/* After the following line of code, pointIdx should be (-1).
+           But there is no sign of it......
+		   Ask your advisor to CONFIRM this.
+		 */
+		 /* Definition in the initRandomTree, */
+		 /* Clue: (*keypointList)[noOfPoint-1]->link = -1; */
+		pointIdx = (*keypointList)[pointIdx]->link;  /* Then pointIdx == -1 */
 	}
     //..
 #if 0
@@ -74,13 +98,15 @@ int getNodeEntropy(pslNode* node){
 int initRandomTree(){
 
 	//..init the point list first
-	int noOfPoint = (int)keypointList->size();
+	/* noOfPoint ----> Number of Points..... */
+	/* Should have a more clearer description!!! */
+	int noOfPoint = (int)keypointList->size();  /* Get the number */
     for(int i = 0; i <noOfPoint-1; i++){
 		(*keypointList)[i]->link = i+1;
 	};
 	(*keypointList)[noOfPoint-1]->link = -1;
 
-	//..init the tree
+	//..init the tree Ye, Initialization.
 	gbTree = allocateNode();
 	gbTree->level    = 0;
 	gbTree->pointNum = (int)keypointList->size();
@@ -90,14 +116,23 @@ int initRandomTree(){
 	return 1;
 };
 
+/*
+  Core function of these files. The princile is easy to understand,
+  you might as well draw some graphs BY-HAND to help
+  figure the process out.
+ */
+
 int splitCurrentNode(pslNode* node){
 
+    /* Display which node to be split */
 	printf("split node:%05d\r", node->nodeID);
-
+    
+	/* Get the number of Feature, why would he use no == num */
 	int noOfFeature = (int)featureList->size();
 
 	//..Record the best feature for split
-	float maxEntropyGrain = -100000.f;
+	/* Record and later write these data into the new node */
+	float maxEntropyGain = -100000.f;
 	int   maxIdx;
 	float maxLUT0[LUT_BIN_NUM]; 
 	float maxLut0Cnt;
@@ -107,14 +142,22 @@ int splitCurrentNode(pslNode* node){
 	float maxEntropy1;
 
 	//..find the best feature for split
+	/* When dealing with Data Structure like this,
+       MATLAB would be a much better choice.
+	 */
 	for(int i = 0; i < noOfFeature; i++){
-		float LUT0[LUT_BIN_NUM]; float lut0Cnt = 0;
-        float LUT1[LUT_BIN_NUM]; float lut1Cnt = 0;
+	    /* Initialization */
+		float LUT0[LUT_BIN_NUM]; 
+		float lut0Cnt = 0;
+        float LUT1[LUT_BIN_NUM]; 
+		float lut1Cnt = 0;
 		memset(LUT0, 0, LUT_BIN_NUM*sizeof(float));
 		memset(LUT1, 0, LUT_BIN_NUM*sizeof(float));
-
+        
+		/* Beginning of the Split process */
 		int pointIdx = node->pointLink;
 		while(pointIdx >= 0){
+		/* Save those data, load later */
 			int featureIdx = i;
 
 			//unsigned char feaVal = feaMat[pointIdx*noOfFeature+featureIdx];
@@ -134,9 +177,11 @@ int splitCurrentNode(pslNode* node){
 		float entropy1 = getEntropyLUT(LUT1, LUT_BIN_NUM, lut1Cnt);
 		float allCnt   = (float)node->pointNum;
 		float entropyGain = node->entropy - ((lut0Cnt/allCnt)*entropy0 + (lut1Cnt/allCnt)*entropy1);
-
-		if(entropyGain > maxEntropyGrain){
-			maxEntropyGrain = entropyGain;
+        
+		/* Compare the requirements (or rather standards) to see */
+		/* whether to split or not                               */
+		if(entropyGain > maxEntropyGain){
+			maxEntropyGain = entropyGain;
 			maxIdx = i;
 			maxLut0Cnt = lut0Cnt;
 			maxLut1Cnt = lut1Cnt;
@@ -148,13 +193,15 @@ int splitCurrentNode(pslNode* node){
 	}
 
 	//..if no entropygain, do not split
-	if(maxEntropyGrain <= 0) return 0;
+	if(maxEntropyGain <= 0) return 0;
 
 	//..get the points for two childs
 	//int* lut0p = new int[(int)maxLut0Cnt]; int lut0idx = 0;
-	int* lut0p = (int*)malloc(((int)maxLut0Cnt)*sizeof(int)); int lut0idx = 0;
+	int* lut0p = (int*)malloc(((int)maxLut0Cnt)*sizeof(int)); 
+	int lut0idx = 0;
 	//int* lut1p = new int[(int)maxLut1Cnt]; int lut1idx = 0;
-	int* lut1p = (int*)malloc(((int)maxLut1Cnt)*sizeof(int)); int lut1idx = 0;
+	int* lut1p = (int*)malloc(((int)maxLut1Cnt)*sizeof(int)); 
+	int lut1idx = 0;
 
 	int pointIdx = node->pointLink;
 	int featureIdx = maxIdx;
@@ -187,6 +234,18 @@ int splitCurrentNode(pslNode* node){
 	node->isLeaf = 0;
 	node->pointLink = -1;
 	
+	
+	/*
+	  My suggestion: Find some other easier/similar codes on
+	  TREE (the specific data structure), or http://baike.baidu.com
+	  or WiKi it for some more information before Reviewing these
+	  following codes.
+	 */
+	 /* 
+	   The above one block and the update child code block are
+	   the VIPs of this function. Read 3-5 more times to digest it.
+	   It's my advice.
+	  */
 	//..update the child0
 	child0->level = node->level+1;
     memcpy(child0->LUT, maxLUT0, sizeof(float)*LUT_BIN_NUM);
@@ -196,6 +255,7 @@ int splitCurrentNode(pslNode* node){
 	for(int i = 0; i < (int)maxLut0Cnt-1; i++){
 		(*keypointList)[lut0p[i]]->link = lut0p[i+1];
 	}
+	
 	(*keypointList)[lut0p[(int)maxLut0Cnt-1]]->link = -1;
 
 	//..update the child1
@@ -220,17 +280,22 @@ int splitCurrentNode(pslNode* node){
 
 
 int splitNode(pslNode* node){
-
+    
+	/* Conditions for not splitting. */
 	if(!node||node->level >= TREE_MAX_LEVEL||
 		node->pointNum <= TREE_MIN_POINT_NUM||
 		node->entropy  <= TREE_ENTROPY_THRESH
 		){
 			return 0;
 	}
-
+    
+	/* Otherwise splitNode */
+	/* 这里面是一个函数的递归调用 */
+	/* 递归程序的经典就是：汉诺塔了...... */
 	//..split current node
 	if(splitCurrentNode(node)){
 		//..split the child node
+		/* Recursion */
 		splitNode(node->Child0);
 		splitNode(node->Child1);
 	}
@@ -242,11 +307,16 @@ int splitNode(pslNode* node){
    Note: build the random tree, the root of the tree is: _tree_
 */
 int buildRandomTree(){
+    /* 
+	  gbTree seems to be the raw material, and 
+	  calling function splitNode() could generate a 
+	  random tree. (This is your algorithm, I've no clue.)
+	 */
 	splitNode(gbTree);
 	return 1;
 };
 
-
+/* This is some clean & clear with memory */
 /* STEP: 5
    Clear the random tree
 */
@@ -297,7 +367,7 @@ int dummyTest(){
 };
 
 
-
+/* File operations. To save/load the data computed. */
 bool saveFeatureList(pslFeatureList* feaList ,char* filename){
 	FILE* fw = fopen(filename, "wb");
 
--- randForest.h
+++ randForest.h
@@ -1,3 +1,31 @@
+/*
+  Briefly, this and its corresponding randForest.cpp
+  file do one thing---->Construct a tree (a very important
+  & MUST-KNOW data structure), and when some requirements
+  are met, say one tree's point number is smaller than 1,000,
+  this tree does NOT have to split into two.
+  
+  Or, when the Entropy (熵, as you have heard of in Math, I guess
+  it's the probability course.) is less than 0.5, the tree does
+  not have to split into two.
+  
+  Otherwise, it has to split. And  the CORE function in randForest.cpp
+  ----int SplitCurrentNode()----
+  does this job.
+  
+  20130426 XY511 10:25
+  AbrahamX @ NWPU
+ */
+
+/*
+  What disgusted me was that YOUR team members do not
+  follow those good naming or definition tranditions 
+  in the Programming field, so it seems weird the
+  first few times I read the codes. Though your algorithms
+  in AI may have special namings (VERY common among the science
+  field......Have to admit. 就是，各个学科对同一事物的命名不一样，
+  体现在乱七八糟的术语上面..........)
+ */
 #ifndef _RANDFOREST_H_
 #define _RANDFOREST_H_
 
@@ -6,9 +34,16 @@
 
 using namespace cv;
 
+
+/* Some macros are defined below. */
 #define LUT_BIN_NUM    SKE_JOINT_NUM
 #define EPS            1e-8
 
+/* 
+  Three distinct & INDEPENDENT requirements,
+  (Level, Point number & Entropy threshold)either one of 
+  them is met, the TREE will split into two.
+ */
 #define TREE_MAX_LEVEL 8		    //max level of tree
 
 #define TREE_MIN_POINT_NUM   1000   //the min number of point for each node,
@@ -21,12 +56,18 @@ using namespace cv;
 /*
   Node structure for the random tree
 */
+
+/*
+  Suggestion: POWER-OFF your laptop and DRAW a picture of this structure
+  pslNode. This is Extremely important for you to FULLY understand
+  the structure being used, in this program.
+ */
 typedef struct pslNode{
 
 	int nodeID;
-	int   feaIdx;
+	int feaIdx;  /* Idx means Index */
 	pslFeature fea;
-	float LUT[LUT_BIN_NUM];	//the probability distribution
+	float LUT[LUT_BIN_NUM];	    //the probability distribution
 	float entropy;				//the entropy of the LUT
 	int   pointNum;				//the number of pixels in this subtree
 	int   level;				//the level of current node
@@ -34,7 +75,7 @@ typedef struct pslNode{
 	int   pointLink;			//the link to the points list of this subtree
 	                            //is valid only for the leaf node
 
-	pslNode* Child0;
+	pslNode* Child0;  /* They should have names like left/right child */
 	pslNode* Child1;
 
 	pslNode(){
@@ -47,12 +88,14 @@ typedef struct pslNode{
 		Child1 = 0;
 		isLeaf = 1;
 		pointLink = -1;
-	}
+	}  /* Destructor (or the Initialization
+	    ), I spuppose. For we don't have C++ course*/
 } pslNode;
 
 typedef vector<pslNode*> pslNodeList;
 
 
+/* Reference, to be used in this program */
 //....
 extern pslFeatureList  *featureList;
 extern pslKeypointList *keypointList;
@@ -60,7 +103,7 @@ extern bitsMat   *feaMat;
 extern pslNode *gbTree;
 //....
 
-
+/* They are for you to accomplish, maybe. */
 
 /* STEP: 3
    Init Random tree
